ac9ff7ac5aaa9f3462c164d6d795abfeebd915fe
rus1an-bagautd1noff <rus1an.bagautd1noff@gmail.com>
2025-08-23 16:58:58 +0500

    Добавил изменения

---
+++ lesson_05/lesson05_task2.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get("http://uitestingplayground.com/dynamicid")
+        
+    wait = WebDriverWait(driver, 5)
+    
+    blue_button = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='Click me']"))
+    )
+    
+    blue_button.click()
+    
+    time.sleep(3)
+    
+finally:
+    driver.quit()
